307e22054878eb2a95c663126c9492e4948fee68
ahmadmuthna <ahmadmuthna4@gmail.com>
2025-03-14 20:58:15 +0300

    refactor: streamline user entity properties and formatting

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,28 +2,26 @@ import { CoreEntity } from "../../common/entities/core.entity";
 import { Column, Entity, Index, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { UserRoleEnum } from "../dto/create-user.dto";
 import { Exclude } from "class-transformer";
-import { Prediction } from "../../prediction/entities/prediction.entity";
 
 @Entity()
 export class User extends CoreEntity {
 
-    
-    @Column({ type: 'varchar', unique: true,nullable:true })
-    email: string;
 
-    @Column({ type: 'varchar',nullable:true })
-    @Index()
-    name: string;
+  @Column({ type: 'varchar', unique: true, nullable: true })
+  email: string;
 
-    @Column({ type: 'varchar' })
-    @Exclude()
-    password: string;
+  @Column({ type: 'varchar', nullable: true })
+  @Index()
+  name: string;
 
-    @Column({ type: 'enum', enum: UserRoleEnum, default: UserRoleEnum.USER })
-    role: UserRoleEnum;
-    
+  @Column({ type: 'varchar' })
+  @Exclude()
+  password: string;
 
-    @OneToMany(() => Prediction, prediction => prediction.team)
-  prediction: Prediction[];
-   
+  @Column({ type: 'enum', enum: UserRoleEnum, default: UserRoleEnum.USER })
+  role: UserRoleEnum;
+
+
+
+
 }
